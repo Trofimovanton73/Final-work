cc3cb9bddbb0c35bcb267b9f69126044b05cdbf5
Trofimov Anton <Trofimovanton73@gmail.com>
2023-01-15 14:48:07 +0300

    Добавляем программу на c#

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+// Примеры: ["hello", "2", "world", ":-)"] -> ["2", ":-)"] 
+// ["1234", "1567", "-2", "computer science"] -> ["-2"] 
+// ["Russia", "Denmark", "Kazan"] -> []
+
+Console.Clear();
+
+int CheckArray(string[] array, int length)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= length) result++;
+        
+    }
+    return result;
+}
+
+void NewArray(string[] oldArray, string[] newArray, int lengthLimit)
+{
+    int temp = 0;
+    for (int i = 0; i < oldArray.Length; i++)
+    {
+        if (oldArray[i].Length <= lengthLimit)
+        {
+            newArray[temp] = oldArray[i];
+            temp++;
+        }
+    }
+}
+
+string PrintArray(string[] array)
+{
+    string result = string.Empty;
+    result = "[ ";
+    for (int i = 0; i < array.Length; i++)
+    {
+        result += $"{array[i],1}";
+        if (i < array.Length - 1) result += ", ";
+    }
+    result += " ]";
+    return result;
+}
+
+string[] ArrayStrings = new string[] { };
+Console.Write("Введите количество строк: ");
+int n = int.Parse(Console.ReadLine());
+ArrayStrings = new string[n];
+for (int i = 0; i < ArrayStrings.Length; i++)
+{
+    Console.Write($" Введите {i + 1} строку: ");
+    ArrayStrings[i] = Console.ReadLine();
+}
+Console.Write("Имеющейся массив:");
+Console.WriteLine($"{PrintArray(ArrayStrings)}");
+int length = 3; 
+int numbers = CheckArray(ArrayStrings, length); 
+string[] newArrayStrings = new string[numbers];
+NewArray(ArrayStrings, newArrayStrings, length);
+Console.Write("Строки массива у которых меньше либо равна 3 символа:");
+Console.WriteLine($"{PrintArray(newArrayStrings)}");
